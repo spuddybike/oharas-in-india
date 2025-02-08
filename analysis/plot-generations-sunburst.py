4bca011871a340384bd3c4c2de007dc57f2e31c9
Jon Johnson <spuddybike@gmail.com>
2025-02-08 14:53:19 +0000

    corrected path seperator

--- analysis/plot-generations-sunburst.py
+++ analysis/plot-generations-sunburst.py
@@ -25,7 +25,7 @@ if not os.path.exists("images"):
 
 
 
-df = pd.read_csv("bmd-data\bmds-generation.csv").query('Descendant == "John"')
+df = pd.read_csv("bmd-data/bmds-generation.csv").query('Descendant == "John"')
 fig4=px.sunburst(df, path=['Descendant','Generation',"Birth Region","Death Region"], values='count', color='Birth Region',
                  color_discrete_map = {
                     'India': '#b45248', # indiared
@@ -45,7 +45,7 @@ fig4.update_traces(textfont=dict(family="Arial", size=8),insidetextorientation='
 fig4.show()
 fig4.write_image("images/bmds-john.png")
 
-df = pd.read_csv("bmd-data\bmds-generation.csv").query('Descendant == "George"')
+df = pd.read_csv("bmd-data/bmds-generation.csv").query('Descendant == "George"')
 fig5=px.sunburst(df, path=['Descendant','Generation',"Birth Region","Death Region"], values='count', color='Birth Region',
                  color_discrete_map = {
                     'India': '#b45248', # indiared
@@ -64,7 +64,7 @@ fig5.update_traces(textfont=dict(family="Arial", size=8),insidetextorientation='
 fig5.show()
 fig5.write_image("images/bmds-george.png")
 
-df = pd.read_csv("bmd-data\bmds-generation.csv").query('Descendant == "Amelia"')
+df = pd.read_csv("bmd-data/bmds-generation.csv").query('Descendant == "Amelia"')
 fig4=px.sunburst(df, path=['Descendant','Generation',"Birth Region","Death Region"], values='count', color='Birth Region',
                  color_discrete_map = {
                     'India': '#b45248', # indiared
@@ -83,7 +83,7 @@ fig4.update_traces(textfont=dict(family="Arial", size=8),insidetextorientation='
 fig4.show()
 fig4.write_image("images/bmds-amelia.png")
 
-df = pd.read_csv("bmd-data\bmds-generation.csv").query('Descendant == "Elizabeth"')
+df = pd.read_csv("bmd-data/bmds-generation.csv").query('Descendant == "Elizabeth"')
 fig5=px.sunburst(df, path=['Descendant','Generation',"Birth Region","Death Region"], values='count', color='Birth Region',
                  color_discrete_map = {
                     'India': '#b45248', # indiared
@@ -103,4 +103,3 @@ fig5.show()
 fig5.write_image("images/bmds-elizabeth.png")
 ## fig4.update_layout(title_text='EDDI Participants 2008-2024', title_x=0.5)
 ## fig4.update_traces(textfont=dict(family="TimesRoman", size=9),insidetextorientation='radial')
-
